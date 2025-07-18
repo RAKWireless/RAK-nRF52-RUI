e9611cc94a490b749da6879174943c92690fa101
ian.lo <ian.lo@rakwireless.com>
2025-07-18 19:56:33 +0800

    modified NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE and NRF_SDH_BLE_VS_UUID_COUNT for fixing memory issue for trackit

--- variants/WisCore_RAK4631_Board/sdk_config.h
+++ variants/WisCore_RAK4631_Board/sdk_config.h
@@ -11744,24 +11744,24 @@
 
 // <o> NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE - Attribute Table size in bytes. The size must be a multiple of 4. 
 #ifndef NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE
-#define NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE 1408
+#define NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE 2048
 #endif
 
 // <o> NRF_SDH_BLE_VS_UUID_COUNT - The number of vendor-specific UUIDs. 
 #ifndef NRF_SDH_BLE_VS_UUID_COUNT
-#define NRF_SDH_BLE_VS_UUID_COUNT 21
+#define NRF_SDH_BLE_VS_UUID_COUNT 30
 #endif
 
 #else
     
 // <o> NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE - Attribute Table size in bytes. The size must be a multiple of 4. 
 #ifndef NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE
-#define NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE 1408
+#define NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE 2048
 #endif
 
 // <o> NRF_SDH_BLE_VS_UUID_COUNT - The number of vendor-specific UUIDs. 
 #ifndef NRF_SDH_BLE_VS_UUID_COUNT
-#define NRF_SDH_BLE_VS_UUID_COUNT 21 //Default 2 ,change from HID implement
+#define NRF_SDH_BLE_VS_UUID_COUNT 30 //Default 2 ,change from HID implement
 #endif
 
 #endif
